8960afb0d7ca830336c1aae4b07c97ba44775e3f
ito-ito <ittooco@gmail.com>
2026-04-26 12:17:43 +0900

    bulletを追加

--- template.rb
+++ template.rb
@@ -6,6 +6,7 @@ end
 
 gem_group :development do
   gem "rubocop-rails-omakase", require: false
+  gem "bullet"
 end
 
 after_bundle do
@@ -27,4 +28,38 @@ after_bundle do
     inherit_gem:
       rubocop-rails-omakase: rubocop.yml
   YAML
+
+  # Bulletの設定をdevelopment.rbに追加
+  environment nil, env: "development" do
+    <<~RUBY
+      config.after_initialize do
+        Bullet.enable = true
+        Bullet.rails_logger = true
+      end
+    RUBY
+  end
+
+  # Bulletの設定をtest.rbに追加
+  environment nil, env: "test" do
+    <<~RUBY
+      config.after_initialize do
+        Bullet.enable = true
+        Bullet.raise = true
+      end
+    RUBY
+  end
+
+  # BulletのフックをRSpecに組み込む
+  inject_into_file "spec/rails_helper.rb", after: "RSpec.configure do |config|\n" do
+    <<~RUBY
+      if Bullet.enable?
+        config.before(:each) { Bullet.start_request }
+        config.after(:each) do
+          Bullet.perform_out_of_channel_notifications if Bullet.notification?
+          Bullet.end_request
+        end
+      end
+
+    RUBY
+  end
 end
